26d80f69884d95c9af533ed84d0db6fc05093c8f
Dx55 <45577743+Dx55@users.noreply.github.com>
2019-06-15 18:54:46 +0200

    Add files via upload

---
+++ ytdl_nolink.py
@@ -0,0 +1,29 @@
+from __future__ import unicode_literals
+import youtube_dl
+import urllib.request
+import urllib.parse
+import re
+
+name = input("Inserisci il nome\n")
+
+print("Attendere...")
+
+query_string = urllib.parse.urlencode({"search_query" : name})
+html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+result = ("http://www.youtube.com/watch?v=" + search_results[0])
+
+ydl_opts ={
+    'format': 'bestaudio/best',
+    'postprocessors':
+    [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([result])
+
+print("Finito")
