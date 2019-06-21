141c8853a02d5698ee56542abfe174e7752eb861
Dx55 <45577743+Dx55@users.noreply.github.com>
2019-06-21 02:43:50 +0200

    Update ytdl_nolink.py

--- ytdl_nolink.py
+++ ytdl_nolink.py
@@ -1,19 +1,22 @@
+#By Dx55
+
 from __future__ import unicode_literals
 import youtube_dl
 import urllib.request
 import urllib.parse
 import re
 
-name = input("Enter the name\n")
-
-print("Wait...")
-
-query_string = urllib.parse.urlencode({"search_query" : name})
-html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
-search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-result = ("http://www.youtube.com/watch?v=" + search_results[0])
-
-ydl_opts ={
+while True:
+    name = input("Enter the name\n")
+    
+    print("Wait...")
+    
+    query_string = urllib.parse.urlencode({"search_query" : name})
+    html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+    search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+    result = ("http://www.youtube.com/watch?v=" + search_results[0])
+    
+    ydl_opts = {
     'format': 'bestaudio/best',
     'postprocessors':
     [{
@@ -21,9 +24,10 @@ ydl_opts ={
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
-}
-
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([result])
-
-print("Finished")
+    }
+    
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([result])
+    
+    if input("Another one? (y/n)") == 'n':
+        break
